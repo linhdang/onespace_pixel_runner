388f8d239c592b7b1f1250f76095755c8f2726a4
Linh Dang <linh.danghong@gmail.com>
2024-08-18 22:45:16 +0100

    lesson 8: add player

--- game_runner.py
+++ game_runner.py
@@ -1,10 +1,6 @@
 import pygame
-from sys import exit
-from random import choice
 
-from obstacle import Obstacle
 from pixel_runner import PixelRunner
-from player import Player
 
 if __name__ == "__main__":
     pygame.init()
